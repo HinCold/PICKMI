9e972d7ed0ac2d185860ac6e40c65a51e0f16560
software-engineering-interviews <48322426+HinCold@users.noreply.github.com>
2019-11-11 21:20:28 +0800

    整合接口， 新增评价功能

--- server/serverapi.py
+++ server/serverapi.py
@@ -1,100 +1,218 @@
-from flask import Flask,request
+from fun import *
+import os
+from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify
 from flask import request
 import time
-from flask_request_params import bind_request_params
 import json
 import pymssql
-user = 'test'
-pwd = 'test'
+from dbtest import PYSQL
+import uuid
+
 app = Flask(__name__)
-order_number = 1
+
 # 查询是否有订单未完成
 # 反馈实时位置
 # 查询订单状态
-
+# 评价
+@app.route('/remark', methods=['POST'])
+def remark():
+    ident = request.form.get('ide')
+    oid = request.form.get('oid')
+    remark = request.form.get('remark')
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    if ident == 'passager':
+        sql = "insert into order(comment1) values('{}') where orderid='{}'".format(remark, oid)
+    else:
+        sql = "insert into order(comment2) values('{}') where orderid='{}'".format(remark, oid)
+    result = my.insert_date(sql)
+    msg = 'success'
+    res = {
+        "status": 0,
+        "msg": msg
+    }
+    my.close()
+    return json.dumps(res)
 # passager get driver data 乘客获取司机信息
 @app.route('/quikcar/data')
 def getquikcar_data():
+    oid = request.args.get("oid")
     '''
     requery database
     '''
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "SELECT dusername FROM orders WHERE orderid = '{}'".format(oid)
+    did1 = my.select_data(sql)
+    did2 = did1[0]
+    did = did2[0]
+    sql = "SELECT dname,dsex,year(getdate())-year(dbirth),cycletype,dtel FROM driver WHERE dusername = '{}'".format(did)
+    suer1 = my.select_data(sql)
+    suer = suer1[0]
     res = {
         "status": 0,
-        "data": {}
+        "data": {
+            "msg": "seccess",
+            "username": did,
+            "name": suer[0],
+            "sex": suer[1],
+            "age": suer[2],
+            "type": suer[3],
+            "tel":suer[4]
+        }
     }
-    return json.dumps(res)
+    return json.dumps(res, ensure_ascii=False)
 # driver take quik car order 司机接快车订单
 @app.route('/driver/quikcar/tkod')
 def takeQcOrder():
-    oid = request.form.get("oid")
-    driver_id = request.form.get("did")
+    oid = request.args.get("oid")
+    driverid = request.args.get("did")
     '''
     updata database
     '''
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "UPDATE orders SET dusername ='{}' WHERE orderid ='{}'".format(driverid, oid)
+    my.update_data(sql)
+    sql = "SELECT susername FROM orders WHERE orderid = '{}'".format(oid)
+    susername1 = my.select_data(sql)
+    susername2 = susername1[0]
+    susername = susername2[0]
+    sql = "SELECT sname,ssex,year(getdate())-year(sbirth),stel FROM student WHERE susername = '{}'".format(susername)
+    suer1 = my.select_data(sql)
+    suer = suer1[0]
     res = {
         "status": 0,
-        "msg": "seccess",
         "data": {
-
+            "msg": "seccess",
+            "username": susername,
+            "name": suer[0],
+            "sex": suer[1],
+            "age": suer[2],
+            "tel": suer[3]
         }
     }
-    return json.dumps(res)
+    return json.dumps(res, ensure_ascii=False)
 
 
-# passager release quik car order 乘客发布快车订单
+# passager release quik car order 乘客发布快车订单  价钱待加
 @app.route('/quikcar/release')
 def releaseQcar():
-   user = request.form.get("user")
-   pos = request.form.get("pos")
-   adress = request.form.get("adr")
-   '''
-   add into database
-   '''
-   res = {
-       "status": 0,
-       "msg": "success"
-   }
-   return json.dumps(res)
+    username = request.args.get("user")
+    startPlace = request.args.get("stp")
+    destination = request.args.get("des")
+    date = request.args.get("date")
+    time = request.args.get("time")
 
-# driver get tail wind car order data 司机获取订单数据
+    '''
+    add into database
+    '''
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "INSERT INTO orders(susername,fast_car,startpoint,endpoint,sdate,stime) VALUES('{}',1,'{}','{}','{}','{}')".format(username,startPlace,destination, date,time)
+    my.insert_date(sql)
+    sql =  "SELECT orderid FROM orders WHERE susername='{}' AND startpoint ='{}' AND endpoint = '{}'AND sdate ='{}'AND stime ='{}'".format(username, startPlace, destination, date,time)
+    orderid1 = my.select_data(sql)
+
+    my.close()
+    orderid2 = orderid1[0]
+    orderid = orderid2[0]
+    res = {
+        "status": 0,
+        "msg": "success",
+        "data": {"oid": orderid}
+        }
+    return json.dumps(res)
+
+# driver get tail wind car order data 司机获取订单数据(order 代表订单类型1是快车，2是顺风车)
 @app.route('/driver/twc/data')
 def getTwc_data():
+    order = request.args.get("order")
+    date = request.args.get("date")
+    time1 = request.args.get("time1")
+    time2 = request.args.get("time2")
+    data =[]
     '''
     requery database
     '''
+    if order ==1:
+        my = PYSQL('localhost', 'test', '123', 'pickme')
+        sql = "SELECT orderid,susername,startpoint,endpoint FROM orders WHERE dusername is NULL AND fast_car = 1 AND  sdate = '{}' AND stime BETWEEN '{}' AND '{}'".format(
+            date, time1, time2)
+        data1 = my.select_data(sql)
+        for vi in data1:
+            susername = vi[1]
+            sql = "SELECT sname ,ssex,year(getdate())-year(sbirth)FROM student WHERE susername = '{}'".format(susername)
+            data2 = my.select_data(sql)
+            data3 = data2[0]
+            data4 = {"oid": vi[0], "startpoint": vi[2], "endpoint": vi[3], "name": data3[0], "sex": data3[1],
+                     "age": data3[2]}
+            data.append(data4)
+    else:
+        my = PYSQL('localhost', 'test', '123', 'pickme')
+        sql = "SELECT orderid,susername,startpoint,endpoint FROM orders WHERE dusername is NULL AND free_ride = 1 AND  sdate = '{}' AND stime BETWEEN '{}' AND '{}'".format(
+            date, time1, time2)
+        data1 = my.select_data(sql)
+        for vi in data1:
+            susername = vi[1]
+            sql = "SELECT sname ,ssex,year(getdate())-year(sbirth)FROM student WHERE susername = '{}'".format(susername)
+            data2 = my.select_data(sql)
+            data3 = data2[0]
+            data4 = {"oid": vi[0], "startpoint": vi[2], "endpoint": vi[3], "name": data3[0], "sex": data3[1],
+                     "age": data3[2]}
+            data.append(data4)
     res = {
         "status": 0,
-        "data": {}
+        "data": data
     }
-    return json.dumps(res)
+    return json.dumps(res, ensure_ascii=False)
 # driver take tail wind car order 司机接顺风车订单并返回乘客信息
 @app.route('/driver/twc/tkod', methods=['POST'])
 def takeTwcOrder():
     oid = request.form.get("oid")
-    driver_id = request.form.get("did")
+    driverid = request.form.get("did")
     '''
     updata database
     '''
-
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "UPDATE orders SET dusername ='{}' WHERE orderid ='{}'".format(driverid,oid)
+    my.update_data(sql)
+    sql = "SELECT susername FROM orders WHERE orderid = '{}'".format(oid)
+    susername1 = my.select_data(sql)
+    susername2 =susername1[0]
+    susername = susername2[0]
+    sql = "SELECT sname,ssex,year(getdate())-year(sbirth),stel FROM student WHERE susername = '{}'".format(susername)
+    suer1 = my.select_data(sql)
+    suer = suer1[0]
     res = {
         "status": 0,
         "data": {
-            "msg": "seccess"
+            "msg": "seccess",
+            "username":susername,
+            "name":suer[0],
+            "sex":suer[1],
+            "age":suer[2],
+            "tel":suer[3]
         }
     }
-    return json.dumps(res)
+    return json.dumps(res, ensure_ascii=False)
 
-# passager Release tail wind car order 乘客发布顺风车订单
+# passager Release tail wind car order 乘客发布顺风车订单   价钱待加
 @app.route('/twcar/release')
 def releaseTwcOrder():
-
-    startPlace = request.form.get("stp")
-    destination = request.form.get("des")
+    username = request.args.get("user")
+    startPlace = request.args.get("stp")
+    destination = request.args.get("des")
+    date = request.args.get("date")
+    time = request.args.get("time")
     '''
     add into database
     '''
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "INSERT INTO orders(susername,free_ride,startpoint,endpoint,sdate,stime) VALUES('{}',1,'{}','{}','{}','{}')".format(username,startPlace,destination, date,time)
+    my.insert_date(sql)
+    sql =  "SELECT orderid FROM orders WHERE susername='{}' AND startpoint ='{}' AND endpoint = '{}'AND sdate ='{}'AND stime ='{}'".format(username, startPlace, destination, date,time)
+    orderid1 = my.select_data(sql)
 
-    orderid = 1
+    my.close()
+    orderid2 = orderid1[0]
+    orderid = orderid2[0]
     res = {
         "status": 0,
         "msg": "success",
@@ -111,11 +229,19 @@ def get_pos():
 # refund 退押金
 @app.route('/refund')
 def refund():
-    user = request.form.get("user")
+    oid = request.args.get("oid")
     '''
     query database
     '''
-    deposit = 1
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "SELECT deposit FROM orders WHERE orderid ='{}'".format(oid)
+    deposit1 =my.select_data(sql)
+    sql = "UPDATE orders SET deposit=0 WHERE orderid ='{}'".format(oid)
+    my.update_data(sql)
+    my.close()
+    deposit2 = deposit1[0]
+    deposit = deposit2[0]
+
     if deposit:
         msg = "success return {} yuan to you".format(deposit)
         '''
@@ -132,13 +258,26 @@ def refund():
 # query is_deposit 交押金
 @app.route('/query/deposit')
 def query_deposit():
-    username = request.form.get("user")
+    oid = request.args.get("oid")
+    deposit = request.args.get("deposit")
+    print(oid)
+    print(deposit)
     '''
     query database
     '''
-    deposit = 1
+    if deposit:
+        my = PYSQL('localhost', 'test', '123', 'pickme')
+        sql ="UPDATE orders SET deposit={} WHERE orderid ='{}'".format(deposit, oid)
+        my.update_data(sql)
+        my.close()
+        status = 0
+        msg = 'success'
+    else:
+        status = -1
+        msg = 'fail'
     res = {
-        "status": 0,
+        "status": status,
+        "msg":msg,
         "data": {"is_deposit": deposit}
     }
     return json.dumps(res)
@@ -148,47 +287,128 @@ def query_deposit():
 @app.route('/deposit')
 def deposit():
 
-    username = request.form.get("user")
-
+    oid = request.args.get("oid") #订单号
+    print(oid)
     '''
     add into database
     '''
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "SELECT deposit FROM orders WHERE orderid ='{}'".format(oid)
+    data1 = my.select_data(sql)
+    my.close()
+    if data1:
+        msg = 'success'
+        status = 0
+    else:
+        msg = 'fail'
+        status = -1
     res = {
-        "status": 0,
-        "msg": "deposit success"
+        "status": status,
+        "msg": msg
     }
     return json.dumps(res)
+
+
+# student valid
+@app.route('/valid', methods=['POST'])
+def valid():
+    username = request.form.get("user")
+    sname = request.form.get("name")
+    ssex = request.form.get("sex")
+    userid = request.form.get("idnum")
+    sinfo = request.form.get("scard")
+    stel = request.form.get("tel")
+    # idcar photo
+    f = request.files['idcard']
+    useridphoto = upphoto(f)
+    print(useridphoto)
+    # student card photos
+    f1 = request.files['stcard']
+    sidphoto = upphoto(f1)
+    # head photos
+
+    # insert into database
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "insert into student(susername, sname, ssex, userid, useridphoto, sinfo, sidphoto, stel) " \
+          "values('{}','{}','{}','{}','{}','{}','{}','{}')".format(username, sname, ssex, userid, useridphoto, sinfo,
+                                                                   sidphoto, stel)
+    result = my.insert_date(sql)
+    if result:
+        msg = 'success'
+        status = 1
+    else:
+        status = -1
+        sql = "select susername from student where susername='{}'".format(username)
+        result = my.select_data(sql)
+        if result:
+            msg = '该用户已认证'
+        else:
+            sql = "select userid from student where userid='{}'".format(userid)
+            result = my.select_data(sql)
+            if result:
+                msg = '身份证已被使用'
+            else:
+                sql = "select sinfo from student where sinfo='{}'".format(sinfo)
+                result = my.select_data(sql)
+                if result:
+                    msg = '学号已被使用'
+                else:
+                    msg = 'unkown wrong'
+    my.close()
+    # print(msg)
+    res = {
+        "status": status,
+        "msg": msg
+    }
+    return json.dumps(res,ensure_ascii=False)
 
 
 # valid 认证
 @app.route('/driver/valid', methods=['POST'])
 def dri_valid():
-    username = request.form.get("user")
-    name = request.form.get("name")
-    sex = request.form.get("sex")
-    idnum = request.form.get("idnum")
+    dusername = request.form.get("user")
+    dname = request.form.get("name")
+    dsex = request.form.get("sex")
+    userid = request.form.get("idnum")
+    dbirth = request.form.get("birth")
+    dtel = request.form.get("tel")
     # carnum = request.form.get("carnum")
-    con = pymssql.connect('127.0.0.1', 'test', '123', 'pickme')
-    cursor = con.cursor()
-    sql = "SELECT dusername FROM driver where dusername='{}'".format(username)
-    cursor.execute(sql)
-    result = cursor.fetchone()
-    if result == None:
-        msg = 'you have already register'
-    else:
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "insert into driver(dusername, dname, dsex, userid, dbirth, dtel) " \
+          "values('{}','{}','{}','{}','{}','{}')".format(dusername, dname, dsex, userid, dbirth, dtel)
+    result = my.insert_date(sql)
 
-        sql = "SELECT userid FROM driver where userid='{}'".format(idnum)
-        cursor.execute(sql)
-        idout = cursor.fetchone()
-        if idout == None:
-            sql = "insert into driver(dusername,dname,dsex,userid) values('{}','{}','{}','{}')".format(username, name, sex, idnum)
-            con.commit()
-    cursor.close()
+    if result:
+        msg = 'success'
+        status = 1
+    else:
+        status = -1
+        sql = "select dusername from driver where dusername='{}'".format(dusername)
+        result = my.select_data(sql)
+        if result:
+            msg = '该用户已认证'
+        else:
+            sql = "select userid from driver where userid='{}'".format(userid)
+            result = my.select_data(sql)
+            if result:
+                msg = '身份证已被使用'
+            else:
+                '''
+                sql = "select sinfo from student where sinfo='{}'".format(sinfo)
+                result = my.select_data(sql)
+                if result:
+                    msg = '学号已被使用'
+                else:
+                '''
+
+                msg = 'unkown wrong'
+    my.close()
+    # print(msg)
     res = {
-        "status": 0,
-        "msg": "success"
+        "status": status,
+        "msg": msg
     }
-    return json.dumps(res)
+    return json.dumps(res, ensure_ascii=False)
 
 
 # get info 获取用户信息
@@ -235,24 +455,20 @@ def register():
     '''
     add new user into database
     '''
-    con = pymssql.connect('127.0.0.1', 'test', '123', 'pickme')
-    cursor = con.cursor()
-    # print(cursor)
-    sql = "SELECT username FROM users where username='{}'".format(username)
-    cursor.execute(sql)
-    res = cursor.fetchone()
-    if res == None:
-        sql = "insert into users values('{}','{}')".format(username, password)
-        cursor.execute(sql)
-        con.commit()
+    my = PYSQL('localhost', 'test', '123', 'pickme')
+    sql = "insert into users values('{}','{}')".format(username, password)
+    result = my.insert_date(sql)
+    if result:
+        print('success')
+        status = 0
         msg = 'success'
     else:
-           msg = 'username has been registered'
-    # res = cursor.fetchone()
-    # print(res)
-    cursor.close()
+        print('fail')
+        status = -1
+        msg = 'username has been registered'
+    my.close()
     res = {
-        "status": 0,
+        "status": status,
         "msg": msg
     }
     return json.dumps(res)
@@ -263,6 +479,7 @@ def register():
 def login():
     username = request.form.get('user')
     password = request.form.get('pwd')
+
     # identify = request.form.get('identify')
     if username == None or password == None:
         res = {
@@ -273,29 +490,26 @@ def login():
     '''
     query database
     '''
-
-    con = pymssql.connect('127.0.0.1', 'test', '123', 'pickme')
-    cursor = con.cursor()
-    # print(cursor)
-    cursor.execute("SELECT username,userpwd FROM users")
-    res = cursor.fetchone()
     # print(res)
-    user = res[0].strip()
-    pwd = res[1].strip()
+    my = PYSQL('localhost', 'test', '123', 'pickme')
 
-    cursor.close()
-    if username == user and password == pwd:
+    sql = "SELECT username,userpwd FROM users where username='{}' and userpwd='{}'".format(username, password)
+    result = my.select_data(sql)
+    print(result)
+    if len(result):
         res = {
-            "status": 0,
+            "status": 1,
             "msg": "success"
+
         }
-        return json.dumps(res)
     else:
         res = {
-            "status": 0,
+            "status": -1,
             "msg": "username or password is not match"
         }
-        return json.dumps(res)
+
+
+    return json.dumps(res)
 
 
 @app.route('/',methods=['GET', 'POST'])
@@ -306,8 +520,26 @@ def hello_world():
 
     #print(type(j))
     # print(json.loads(s))
-    return 'Hello World!\nwelcome to PICKME'
+    return '欢迎使用PICKME'
 
+@app.route('/upload', methods=['POST', 'GET'])  # 添加路由
+def upload():
+    # print(request)
+    if request.method == 'POST':
+
+        f = request.files['file']
+        if not (f and allowed_file(f.filename)):
+            return jsonify({"error": 1001, "msg": "请检查上传的图片类型，仅限于png、PNG、jpg、JPG、bmp"})
+        basepath = os.path.dirname(__file__)  # 当前文件所在路径
+        print(basepath)
+        src_imgname = str(uuid.uuid1()) + ".jpg"
+        upload_path = os.path.join(basepath, 'images/')
+        if os.path.exists(upload_path) == False:
+            os.makedirs(upload_path)
+        print(upload_path + src_imgname)
+        f.save(upload_path + src_imgname)
+
+        return 'ok'
 
 if __name__ == '__main__':
     # app.run(host="www.pickmi.club", port=8090)
